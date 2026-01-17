7259ecb01bb7cf498a36264243c6d1fcdea5bb9b
paJavier <pajavier@mcm.edu.ph>
2026-01-17 23:24:12 +0800

    add metatag for GSC

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="ABC123xyz" />
+        {/* other head tags */}
+      </head>
+
       <body className="bg-white text-blue-900 antialiased">
         {children}
       </body>
